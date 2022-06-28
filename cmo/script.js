0516ab0dd00f2004a0713377fe71bd3cd05bbe9f
Chethan Krishna G R <71527515+chethankrishnagr@users.noreply.github.com>
2022-06-29 05:27:22 +0530

    Link to Sheets

--- cmo/script.js
+++ cmo/script.js
@@ -1,21 +1,44 @@
-// const spreadsheet_id = '1Fzh7ydeKvfijcE5vfdI5nFARmUXBMbvxxiTvfhbSXc8';
-// const api_key = AIzaSyCQBDU815Mh86-Z9FGIVCtL0cQB9Tg9l3I;
+const list = document.querySelector('#mainFrame');
 
-// console.log(spreadsheet_id);
-
-// var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheet_id + '/values/' + tab_name + '?alt=json&key=' + api_key;
-var url = 'https://sheets.googleapis.com/v4/spreadsheets/1Fzh7ydeKvfijcE5vfdI5nFARmUXBMbvxxiTvfhbSXc8/values/General_Medicine?alt=json&key=AIzaSyCQBDU815Mh86-Z9FGIVCtL0cQB9Tg9l3I'
-// ($.getJSON(url, 'callback=?')).success(function(data){
-//     console.log(data);
-// })
+const id = new URLSearchParams(window.location.search).get('id');
+// const id = "genmed";
 
 let request = new XMLHttpRequest();
-request.open('GET', url);
+request.open('GET', 'name.json');
 request.responseType = 'json';
 request.send();
-
 request.onload = function () {
-  const fullDetails = request.response;
-  console.log(fullDetails);
-//   alert('Girish is watching');
-} 
+  const metaData = request.response;
+  const meta = metaData.filter(arr => arr.link == id)[0];
+  const title = meta.name + " | Contact Details";
+  document.querySelector('title').textContent = title;
+  const tab_name = meta.tab_name;
+  load();
+
+  function load(){
+    let spreadsheet_id = '1Fzh7ydeKvfijcE5vfdI5nFARmUXBMbvxxiTvfhbSXc8';
+    let api_key = 'AIzaSyBDMPIplcIRfbN4Fh5K8dmF9IN_4vP6b8Y';
+    let requestURL = 'https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheet_id + '/values/' + tab_name + '?alt=json&key=' + api_key;
+    
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function () {
+    const fullData = request.response;
+    const data = fullData.values;
+    list(data);
+    }
+
+    function list(obj){
+      var list = '';
+      for (let i = 1; i < obj.length; i++) {
+        list += '<div class="links"><a class="links links--' + obj[i][4] + '" href="' + obj[i][3] + '">' + obj[i][0] + '</a></div>';
+      }
+      document.querySelector('#mainFrame').innerHTML = list;
+      console.log(obj);    
+    }
+
+
+  }
+}
