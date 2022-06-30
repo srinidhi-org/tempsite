c76173379f92ac0cbd8cc25548e7c8523b588a3b
Chethan Krishna G R <chethankrishnagr@yahoo.com>
2022-06-30 07:44:31 +0530

    Complete flexibility for Sheets

--- cmo/script.js
+++ cmo/script.js
@@ -1,7 +1,6 @@
 const list = document.querySelector('#mainFrame');
 
 const id = new URLSearchParams(window.location.search).get('id');
-// const id = "genmed";
 
 let request = new XMLHttpRequest();
 request.open('GET', 'name.json');
@@ -26,19 +25,44 @@ request.onload = function () {
     request.send();
     request.onload = function () {
     const fullData = request.response;
-    const data = fullData.values;
+    const data = fullData.values.filter(arr => arr[0] != "");
     list(data);
     }
 
     function list(obj){
       var list = '';
+      var colour;
+      var phonenum;
+      var name;
       for (let i = 1; i < obj.length; i++) {
-        list += '<div class="links"><a class="links links--' + obj[i][4] + '" href="' + obj[i][3] + '">' + obj[i][0] + '</a></div>';
+        switch (obj[i][1]) {
+          case 'Intern':
+            colour = 'canyan';
+            break;
+          case 'PG':
+            colour = 'light-yellow';
+            break;
+          case 'SR':
+            colour = 'light-red';
+            break;
+          case 'SRID':
+            colour = 'light-red';
+            break;        
+          default: colour = 'grey';
+            break;
+        }
+        if (isNaN(obj[i][2])){
+          phonenum = obj[i][2] ;
+        } else {
+          phonenum = `tel:${obj[i][2]}` ;
+        }
+        name = obj[i][0] ;
+        list += `<div class="links"><a class="links links--${colour}" href="${phonenum}">${name}</a></div>`;
       }
       document.querySelector('#mainFrame').innerHTML = list;
-      console.log(obj);    
+      document.getElementById("departments").style.display = "none";
+      console.log("Successfully loaded");    
     }
-
 
   }
 }
